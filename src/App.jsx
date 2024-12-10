2afd244164b801ebbc435a6ad1c0743849a37a7d
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-12-10 14:11:53 +0600

    add Marque

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from "./Component/Pages/Navbar/Navbar"
 import LandingPage from './Component/Pages/LandingPage/LandingPage';
+import Marque from './Component/Pages/Marque/Marque';
 
 const App = () => {
   return (
-    <div>
+   
+   <div className='min-h-screen bg-zinc-800'>
       <Router>
 
         <Navbar></Navbar>
 
         <LandingPage></LandingPage>
+
+        <Marque></Marque>
 
       </Router>
     </div>
