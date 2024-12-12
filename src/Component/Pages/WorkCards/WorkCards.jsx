a7c422dfdaa61c8ee80ad88f4862e61fef50835d
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-12-12 16:12:00 +0600

    add WorkCards

--- src/Component/Pages/WorkCards/WorkCards.jsx
+++ src/Component/Pages/WorkCards/WorkCards.jsx
@@ -1,24 +1,28 @@
 
 const WorkCards = () => {
     return (
-        <div className="w-full bg-zinc-100 h-screen flex gap-5 p-32">
+        <div className="w-full bg-zinc-100 h-screen flex flex-col md:flex-row gap-5 py-32 px-10">
 
-            <div className="cardContainer h-[50vh] w-1/2">
-                <div className="card relative w-full h-full bg-[#004d43] rounded-md flex justify-center items-center">
+            <div className="cardContainer md:h-[50vh]  w-full md:w-1/2 px-10 cursor-pointer">
+                <div className="card relative w-full h-[300px] md:h-full bg-[#004d43] rounded-md flex justify-center items-center">
                     <img className="w-32" src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg" alt="" />
-                    <button className="absolute left-10 bottom-10 px-5 py-3  border-2 border-[#CDEA68] rounded-full text-[#CDEA68] font-semibold">&copy;2022-2024</button>
+                    <button className="absolute left-10 bottom-10 px-5 py-1  border-2 border-[#CDEA68] rounded-full text-[#CDEA68] font-semibold">&copy;2022-2024</button>
                 </div>
             </div>
 
-            <div className="cardContainer h-[50vh] w-1/2 flex gap-5">
+            <div className="cardContainer  md:h-[50vh] w-full md:w-1/2 flex gap-5">
 
-                <div className="card relative w-full h-full bg-[#004d43] rounded-md flex justify-center items-center">
+                <div className="card relative w-full h-[300px] md:h-full bg-[#212121] rounded-md flex justify-center items-center cursor-pointer">
                     <img className="w-32" src="https://ochi.design/wp-content/uploads/2022/04/logo002.svg" alt="" />
-                    <button className="absolute left-10 bottom-10 px-5 py-3  border-2 border-[#CDEA68] rounded-full text-[#CDEA68] font-semibold">&copy;2022-2024</button>
+                    <button className="absolute left-10 bottom-10 px-8 py-1  border-2 border-[#CDEA68] rounded-full text-[#CDEA68] font-semibold uppercase">Rating 5.0 on clutch</button>
                 </div>
 
 
-                <div className="card w-full h-full bg-[#004d] rounded-md">                </div>
+                <div className="card relative w-full h-[300px] md:h-full bg-[#212121] rounded-md flex justify-center items-center cursor-pointer">
+                    <img className="w-32" src="https://ochi.design/wp-content/uploads/2022/04/logo003.png" alt="" />
+                    <button className="absolute left-10 bottom-10 px-1 py-1  border-2 border-[#CDEA68] rounded-full text-[#CDEA68] font-semibold uppercase ">business bootcamp alumni</button>
+                </div>
+
             </div>
 
         </div >
